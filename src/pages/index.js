52026a162ebb254a45f380641abe8ff1bf05d216
Ben Franzoso <benfranzoso38@gmail.com>
2020-02-18 21:48:35 -0500

    add section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
+import Steps from "../components/steps"
 
 export default () => (
     <Layout>
         <Hero></Hero>
+        <Steps></Steps>
     </Layout>
 )
