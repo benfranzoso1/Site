35c2acd3dfe72d9b56a5bc50179872162bf96ad6
Ben Franzoso <benfranzoso38@gmail.com>
2020-02-18 22:16:53 -0500

    boxshadow

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import Steps from "../components/steps"
+import Why from "../components/why"
 
 export default () => (
     <Layout>
         <Hero></Hero>
         <Steps></Steps>
+        <Why></Why>
     </Layout>
 )
