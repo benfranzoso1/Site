301cff834b8dd1bc7c33bae0cf8824e48f7fc103
Ben Franzoso <benfranzoso38@gmail.com>
2020-02-21 23:09:55 -0500

    testing hero01

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
+import Hero01 from "../components/hero01"
 import Dealers from "../components/dealers"
 import Steps from "../components/steps"
 import Why from "../components/why"
 
 export default () => (
     <Layout>
+        <Hero01></Hero01>
         <Hero></Hero>
         <Dealers></Dealers>
         <Steps></Steps>
