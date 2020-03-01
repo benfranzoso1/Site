9f237a1c7a8655b0db7009ace8d7f4640cff4984
Ben Franzoso <benfranzoso38@gmail.com>
2020-02-29 19:26:29 -0500

    tabbs

--- src/components/faq.js
+++ src/components/faq.js
@@ -6,15 +6,14 @@ import 'react-tabs/style/react-tabs.css';
 export default () => (
 <section className="faq">
     <div className="container">
-    <Tabs>
-    <TabList>
-      <Tab>Mario</Tab>
-      <Tab>Peach</Tab>
-      <Tab>Yoshi</Tab>
-      <Tab>Toad</Tab>
+    <Tabs className="tabs">
+    <TabList className="tablist">
+      <Tab className="tab">Getting Pre-Qualifed</Tab>
+      <Tab className="tab">How Does It Work?</Tab>
+      <Tab className="tab">What's The Next Step</Tab>
     </TabList>
 
-    <TabPanel>
+    <TabPanel className="tabpanel">
       <p>
         <b>Mario</b> (<i>Japanese: マリオ Hepburn: Mario, [ma.ɾʲi.o]</i>) (<i>English:
         /ˈmɑːrioʊ/; Italian: [ˈmaːrjo]</i>) is a fictional character in the Mario video
@@ -32,7 +31,7 @@ export default () => (
         </a>
       </p>
     </TabPanel>
-    <TabPanel>
+    <TabPanel className="tabpanel">
       <p>
         <b>Luigi</b> (<i>Japanese: ルイージ Hepburn: Ruīji, [ɾɯ.iː.dʑi̥]</i>) (<i>English: /luˈiːdʒi/;
         Italian: [luˈiːdʒi]</i>) is a fictional character featured in video games and related media
@@ -47,7 +46,7 @@ export default () => (
         </a>
       </p>
     </TabPanel>
-    <TabPanel>
+    <TabPanel className="tabpanel">
       <p>
         <b>Princess Peach</b> (<i>Japanese: ピーチ姫 Hepburn: Pīchi-hime, [piː.tɕi̥ çi̥.me]</i>)
         is a character in Nintendo's Mario franchise. Originally created by Shigeru Miyamoto,
@@ -59,42 +58,6 @@ export default () => (
       <p>
         Source:{' '}
         <a href="https://en.wikipedia.org/wiki/Princess_Peach" target="_blank">
-          Wikipedia
-        </a>
-      </p>
-    </TabPanel>
-    <TabPanel>
-      <p>
-        <b>Yoshi</b> (<i>ヨッシー Yosshī, [joɕ.ɕiː]</i>) (<i>English: /ˈjoʊʃi/ or /ˈjɒʃi/</i>), once
-        romanized as Yossy, is a fictional anthropomorphic dinosaur who appears in
-        video games published by Nintendo. Yoshi debuted in Super Mario World (1990) on the
-        Super Nintendo Entertainment System as Mario and Luigi's sidekick. Yoshi later starred
-        in platform and puzzle games, including Super Mario World 2: Yoshi's Island, Yoshi's Story
-        and Yoshi's Woolly World. Yoshi also appears in many of the Mario spin-off games, including
-        Mario Party and Mario Kart, various Mario sports games, and Nintendo's crossover fighting
-        game series Super Smash Bros. Yoshi belongs to the species of the same name, which is
-        characterized by their variety of colors.
-      </p>
-      <p>
-        Source:{' '}
-        <a href="https://en.wikipedia.org/wiki/Yoshi" target="_blank">
-          Wikipedia
-        </a>
-      </p>
-    </TabPanel>
-    <TabPanel>
-      <p>
-        <b>Toad</b> (<i>Japanese: キノピオ Hepburn: Kinopio</i>) is a fictional character who primarily
-        appears in Nintendo's Mario franchise. Created by Japanese video game designer Shigeru Miyamoto,
-        he is portrayed as a citizen of the Mushroom Kingdom and is one of Princess Peach's most loyal
-        attendants; constantly working on her behalf. He is usually seen as a non-player character (NPC)
-        who provides assistance to Mario and his friends in most games, but there are times when Toad(s)
-        takes center stage and appears as a protagonist, as seen in Super Mario Bros. 2, Wario's Woods,
-        Super Mario 3D World, and Captain Toad: Treasure Tracker.
-      </p>
-      <p>
-        Source:{' '}
-        <a href="https://en.wikipedia.org/wiki/Toad_(Nintendo)" target="_blank">
           Wikipedia
         </a>
       </p>
