b2d2de78e7cb96e9502ec01ef468fd9bce9c547c
Ben Franzoso <benfranzoso38@gmail.com>
2020-03-22 16:06:01 -0400

    movie file

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Field } from "react-final-form"
-import Wizard from "./Form/Wizard"
+import Wizard from "./Wizard"
 import phone from "phone"
 import { navigate } from "gatsby"
 import Slider from "rc-slider"
--- src/pages/getstarted.js
+++ src/pages/getstarted.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Form from "../components"
+import Form from "../components/Form"
 
 
 const GetStarted = () => (
